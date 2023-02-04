132442c2bbbbe21eac3c6e0f943c5285af4180a4
Alexandr Tristinskiy <3stan91@gmail.com>
2023-02-04 23:49:55 +0600

    rename variables

--- main.cpp
+++ main.cpp
@@ -8,31 +8,31 @@ using std::string;
 enum class Status {
     INCORRECT, CORRECT, NOBODY, VANYA, PETYA
 };
-constexpr int countOfString = 3;
+constexpr int sizeOfTable = 3;
 
 void inputString(string &);
 
-Status testInput(char table[][countOfString], int);
+Status testInput(char table[][sizeOfTable], int);
 
-bool checking(char table[][countOfString], int);
+bool checking(char table[][sizeOfTable], int);
 
 int main() {
-    string s[countOfString]{"X..", "OXO", "OOO"};
-    /*  for (int i = 0; i < countOfString; ++i)
+    string s[sizeOfTable]{"X..", "OXO", "OOO"};
+    /*  for (int i = 0; i < sizeOfTable; ++i)
           inputString(s[i]);
     */
-    char table[countOfString][countOfString];
+    char table[sizeOfTable][sizeOfTable];
 
-    for (int i = 0; i < countOfString; ++i)
-        for (int j = 0; j < countOfString; ++j)
+    for (int i = 0; i < sizeOfTable; ++i)
+        for (int j = 0; j < sizeOfTable; ++j)
             table[i][j] = s[i][j];
 
-    if (!checking(table, countOfString)) {
+    if (!checking(table, sizeOfTable)) {
         cout << "Incorrect\n";
         exit(1);
     }
 
-    switch (testInput(table, countOfString)) {
+    switch (testInput(table, sizeOfTable)) {
         case Status::CORRECT:
             cout << "Correct\n";
             break;
@@ -52,12 +52,12 @@ int main() {
     return 0;
 }
 
-Status testInput(char table[][3], int numberString) {
+Status testInput(char table[][3], int sizeOfTable) {
     int countDots = 0, countO = 0, countX = 0;
-    // bool nobody = false;
+    bool nobody = false;
     char mas[3];
-     for (int i = 0; i < numberString; ++i) {
-         for (int j = 0; j < numberString; ++j) {
+     for (int i = 0; i < sizeOfTable; ++i) {
+         for (int j = 0; j < sizeOfTable; ++j) {
              if (table[i][j] == '.')
                  ++countDots;
              if (table[i][j] == 'O')
@@ -65,29 +65,28 @@ Status testInput(char table[][3], int numberString) {
              if (table[i][j] == 'X')
                  ++countX;
          }
-     }
 
-      /*   if (countX < countO || countO < countX)
+         if (countX < countO || countO < countX)
              nobody = true;
 
-         if ((countX > countO && countX > 3) || (countX < countO && countO > 3))
+         if ((countX > countO && countX > sizeOfTable) || (countX < countO && countO > sizeOfTable))
              return Status::INCORRECT;
          if (nobody)
              return Status::NOBODY;
 
          if (countO <= countDots && countX <= countDots)
              return Status::NOBODY;
-     */
+     }
 
     int testDialonal = 0;
     while (testDialonal < 2) {
         if (testDialonal == 0) {
-            for (int i = 0; i < numberString; ++i) {
+            for (int i = 0; i < sizeOfTable; ++i) {
                 mas[i] = table[i][i];
             }
         } else
-            for (int i = 0; i < numberString; ++i) {
-                mas[i] = table[i][numberString - i - 1];
+            for (int i = 0; i < sizeOfTable; ++i) {
+                mas[i] = table[i][sizeOfTable - i - 1];
             }
         for (const char c: mas) {
             switch (c) {
@@ -100,15 +99,15 @@ Status testInput(char table[][3], int numberString) {
             }
         }
 
-        if (countO == 3)
+        if (countO == sizeOfTable)
             return Status::VANYA;
-        else if (countX == 3)
+        else if (countX == sizeOfTable)
             return Status::PETYA;
         testDialonal++;
         countX = countO = 0;
     }
 
-    for (int i = 0; i < numberString; ++i) {
+    for (int i = 0; i < sizeOfTable; ++i) {
         if ((table[i][0] == table[i][1] == table[i][2] == 'X') ||
             (table[0][i] == table[1][i] == table[2][i] == 'X'))
             return Status::PETYA;
@@ -118,20 +117,6 @@ Status testInput(char table[][3], int numberString) {
         else
             return Status::NOBODY;
     }
-
-
-    /*      if ((table[i][i] == 'X') || (table[i][numberString - i - 1] == 'X') ||
-              (table[i][0] == table[i][1] && table[i][0] == table[i][2] && table[i][0] == 'X')
-              || (table[0][i] == table[1][i] && table[0][i] == table[2][i] && table[0][i] == 'X'))
-              return Status::PETYA;
-          else if ((table[i][i] == 'O') || (table[i][numberString - i - 1] == 'O') ||
-                   (table[i][0] == table[i][1] && table[i][0] == table[i][2] && table[i][0] == 'O')
-                   || (table[0][i] == table[1][i] && table[0][i] == table[2][i] && table[0][i] == 'O'))
-              return Status::VANYA;
-          else
-              return Status::NOBODY;
-      }
-      */
 }
 
 
